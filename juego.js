351da0fddd59cb414784dc6a49411a9704185f22
monicacaseys <115183037+monicacaseys@users.noreply.github.com>
2023-04-30 16:49:32 +0200

    d

--- juego.js
+++ juego.js
@@ -15,52 +15,52 @@ function drawSymbol(index) {
   const square = document.getElementById(index);
   if (symbols[index] === '') {
     square.textContent = '♘';
-    square.style.fontWeight = 'bold'; 
+    square.style.fontWeight = 'bold';
     symbols[index] = '♘';
     checkWin();
   }
 }
 
-  function checkWin() {
-    const rows = [    [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-    ];
-    const columns = [    [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-    ];
-    const diagonals = [    [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < rows.length; i++) {
-      const row = rows[i];
-      const col = columns[i];
-      if (symbols[row[0]] !== '' && symbols[row[0]] === symbols[row[1]] && symbols[row[1]] === symbols[row[2]]) {
-        alert('¡Has ganado! Si quieres volver a juagr te recomiendo que recargues la pagina');
-        return;
-      }
-      if (symbols[col[0]] !== '' && symbols[col[0]] === symbols[col[1]] && symbols[col[1]] === symbols[col[2]]) {
-        alert('¡Has ganado!Si quieres volver a juagar te recomiendo que recargues la pagina');
-        return;
-      }
-    }
-    for (let i = 0; i < diagonals.length; i++) {
-      const diag = diagonals[i];
-      if (symbols[diag[0]] !== '' && symbols[diag[0]] === symbols[diag[1]] && symbols[diag[1]] === symbols[diag[2]]) {
-        alert('¡Has ganado!Si quieres volver a juagar te recomiendo que recargues la pagina');
-        return;
-      }
-    }
-    if (symbols[0] !== '' && symbols[0] === symbols[4] && symbols[4] === symbols[8]) {
-      alert('¡Has ganado!Si quieres volver a juagar te recomiendo que recargues la pagina');
+function checkWin() {
+  const rows = [[0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  ];
+  const columns = [[0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  ];
+  const diagonals = [[0, 4, 8],
+  [2, 4, 6],
+  ];
+  for (let i = 0; i < rows.length; i++) {
+    const row = rows[i];
+    const col = columns[i];
+    if (symbols[row[0]] !== '' && symbols[row[0]] === symbols[row[1]] && symbols[row[1]] === symbols[row[2]]) {
+      alert('¡Has ganado! Si quieres volver a juagr te recomiendo que recargues la pagina');
       return;
     }
-    if (symbols[2] !== '' && symbols[2] === symbols[4] && symbols[4] === symbols[6]) {
+    if (symbols[col[0]] !== '' && symbols[col[0]] === symbols[col[1]] && symbols[col[1]] === symbols[col[2]]) {
       alert('¡Has ganado!Si quieres volver a juagar te recomiendo que recargues la pagina');
       return;
     }
   }
-  
+  for (let i = 0; i < diagonals.length; i++) {
+    const diag = diagonals[i];
+    if (symbols[diag[0]] !== '' && symbols[diag[0]] === symbols[diag[1]] && symbols[diag[1]] === symbols[diag[2]]) {
+      alert('¡Has ganado!Si quieres volver a juagar te recomiendo que recargues la pagina');
+      return;
+    }
+  }
+  if (symbols[0] !== '' && symbols[0] === symbols[4] && symbols[4] === symbols[8]) {
+    alert('¡Has ganado!Si quieres volver a juagar te recomiendo que recargues la pagina');
+    return;
+  }
+  if (symbols[2] !== '' && symbols[2] === symbols[4] && symbols[4] === symbols[6]) {
+    alert('¡Has ganado!Si quieres volver a juagar te recomiendo que recargues la pagina');
+    return;
+  }
+}
+
 
 
